33ac264b40fef6fc908914a2232242111f1fb927
Fadhilah22 <107631556+Fadhilah22@users.noreply.github.com>
2023-10-28 22:02:52 +0700

    Update compile-restaurant.bat
    replaced ':' with ';'

--- compile-restaurant.bat
+++ compile-restaurant.bat
@@ -1,1 +1,1 @@
-javac -d bin/Restaurant -cp .:bin/Menu:bin/RegularMenu:bin/SpecialMenu  src/Restaurant/Restaurant.java
+javac -d bin/Restaurant -cp .;bin/Menu;bin/RegularMenu;bin/SpecialMenu src/Restaurant/Restaurant.java
